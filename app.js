b3176510b27ebaacc7e0c9eefdeff24034a20889
bilast <129021049+bilast@users.noreply.github.com>
2023-04-17 03:34:56 +0000

    Update app.js

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ function restartDocker() {
 
 function timer() {
   seconds = seconds + 1; // remove 'let' keyword here
-  if (seconds <= 700) {
-    console.log(seconds, "s /700s to restart Docker");
+  if (seconds <= 550) {
+    console.log(seconds, "s /550s to restart Docker");
     setTimeout(timer, 1000);
   }
   else {
